e8cabfe29d5e53d8d5e41d8ede87a8bcb7cc0dc5
Anonymous <suportdevs>
2023-09-24 23:10:47 +0600

    footer section

--- client/src/pages/Layout/Layout.jsx
+++ client/src/pages/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import TopBar from "../../components/TopBar/TopBar";
+import Footer from "../../components/Footer/Footer";
 
 export default function Layout() {
     return(
         <>
         <TopBar />
         {/* <Outlet /> */}
+        <Footer />
         </>
     )
 }
