c4801e0a372048f7dc152bb33aa7bfbfa144dfc2
Anonymous <suportdevs>
2023-09-24 21:28:57 +0600

    update routing using layout

--- client/src/App.js
+++ client/src/App.js
@@ -15,34 +15,41 @@ import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./services/reducer";
 import Contact from "./pages/Contact/Contact";
 import About from "./pages/About/About";
+import Layout from "./pages/Layout/Layout";
 
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <TopBar />,
+    element: <Layout />,
     children: [
       {
-        path: "",
+        path: "/",
         element: <Home />,
       },
       {
-        path: "post/:id",
+        path: "/post/:id",
         element: <SinglePost />,
       },
       {
-        path: "create-post",
+        path: "/create-post",
         element: <Protected><CreatePost /></Protected>,
       },
       {
-        path: "post/edit/:id",
+        path: "/post/edit/:id",
         element: <Protected><PostEdit /></Protected>,
       },
       {
-        path: "create-category",
+        path: "/create-category",
         element: <Protected><CreateCategory /></Protected>,
       },
-      
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
     ],
   },
   {
@@ -53,24 +60,6 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
   },
-  {
-    path: '/contact',
-    element: <TopBar/>,
-    children:[
-      {path: '',
-      element: <Contact />,}
-    ],
-  },
-  {
-    path: '/about',
-    element: <TopBar/>,
-    children:[
-      {
-        path: '',
-      element: <About />,
-    }
-    ],
-  }
 ]);
 
 function App() {
@@ -81,19 +70,9 @@ function App() {
   }, [currentUser]);
 
   return (
-    <RouterProvider router={router} />
-    // <Router>
-    //   <TopBar/>
-    // <Routes>
-    //   <Protected>
-    //   <Route exact path="/" element={<Home />} />
-    //   </Protected>
-    //     <Route path="/post/:id" element={<SinglePost />} />
-    //     <Route path="/create-post" element={<CreatePost />} />
-    //     <Route path="/login" element={<Login />} />
-    //     <Route path="/register" element={<Register />} />
-    // </Routes>
-    // </Router>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
---
+++ client/src/pages/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+import TopBar from "../../components/TopBar/TopBar";
+
+export default function Layout() {
+    return(
+        <>
+        <TopBar />
+        {/* <Outlet /> */}
+        </>
+    )
+}
